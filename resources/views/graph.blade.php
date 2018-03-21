bfe820b5d4810083389bfb9adeae7e994cf99d0b
Jaap van der Vis <jaapvdvis@gmail.com>
2018-03-21 17:20:00 +0100

    updated timetracking

---
+++ resources/views/graph.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section{'content'}
+<div class="graph">
+
+
+
+
+
+
+
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.2.10/cytoscape.js"></script>
+<script src="{{ asset('js/workflow.js') }}"></script>
+
+
+@endsection{'content'}
