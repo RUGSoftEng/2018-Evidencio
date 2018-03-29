7f9543affe6319a3b3905ba7e1c3237c76349953
Dammes <ddezoeten@gmail.com>
2018-03-29 17:00:30 +0200

    Added basic graph

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -2,6 +2,45 @@
 use App\EvidencioAPI;
 if (!empty($_GET['answer'])&&!empty($_GET['model'])) {
   $decodeRes = EvidencioAPI::run($_GET['model'],$_GET['answer']);
-  print_r ($decodeRes);
 }
 ?>
+@extends('layouts.app')
+
+@section('content')
+
+<?php
+$result=(int)$decodeRes['result'];
+$dataPoints = array(
+	array("label"=>$decodeRes['title'], "y"=>$result),
+  array("label"=>"", "y"=>(100-$result)),
+)
+
+?>
+<script>
+window.onload = function() {
+
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title: {
+		text: "<?php echo $decodeRes['title'] ?>"
+	},
+	subtitles: [{
+		text:"Pie Chart"
+	}],
+	data: [{
+		type: "pie",
+		yValueFormatString: "#,##0.00\"%\"",
+		indexLabel: "{label} ({y})",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+
+}
+</script>
+
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+@endsection
