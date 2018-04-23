f52ab07506645e2e46a73cb236d9db5836cea0a1
Dansuf <tomasz.ks9@gmail.com>
2018-04-23 19:18:56 +0200

    Migrations and Models related to running Evidencio API models.

--- app/Field.php
+++ app/Field.php
@@ -6,9 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Field extends Model
 {
+    public $timestamps = false;
 
     public function options()
     {
         return $this->hasMany('App\Option','categoricalFieldId');
+    }
+
+    public function inputSteps()
+    {
+        return $this->belongsToMany('App\Step','field_in_input_steps','fieldId','inputStepId');
+    }
+
+    public function usedInRunsInSteps()
+    {
+        return $this->belongsToMany('App\Step','model_run_field_mappings','fieldId','stepId');
     }
 }
