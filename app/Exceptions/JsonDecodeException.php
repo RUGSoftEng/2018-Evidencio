26bba67df769e00b0215e843fd68a069ca458aa2
Dansuf <tomasz.ks9@gmail.com>
2018-05-01 00:46:59 +0200

    API methods' docblocks extension and code clean-up.

--- app/Exceptions/JsonDecodeException.php
+++ app/Exceptions/JsonDecodeException.php
@@ -4,4 +4,17 @@ namespace App\Exceptions;
 
 use Exception;
 
-class JsonDecodeException extends Exception { }
+class JsonDecodeException extends Exception {
+
+  /*
+   * @param $response the code that could not be decoded
+   * @see Exception
+   */
+  public function __construct($response, $code = 0, Exception $previous = null) {
+
+      $message = "Could not decode API response to JSON: '".$response."'.";
+
+      parent::__construct($message, $code, $previous);
+  }
+
+}
