9b8082dd6cd85fb300bcbadc38084a07ef3409cd
Unknown <jaapvdvis@gmail.com>
2018-05-31 17:30:19 +0200

    Wrong filename

--- app/Http/Controllers/MyWorkflowsController.php
+++ app/Http/Controllers/MyWorkflowsController.php
@@ -17,10 +17,10 @@ class MyWorkflowsController extends Controller
     public function index()
     {
 
-    	$user_id = Auth::user()->id;
-		$workflows = Workflow::where('author_id','=',$user_id)->get();
+        $user_id = Auth::user()->id;
+        $workflows = Workflow::where('author_id', '=', $user_id)->get();
 
-        return view('myworkflows',compact('workflows'));
+        return view('myworkflows', compact('workflows'));
     }
 
     public function deleteWorkflow($id)
@@ -28,31 +28,29 @@ class MyWorkflowsController extends Controller
         /*****delete the verification comments and comment replies*****/
         
         //get the id's of verification comments of given workflow
-        $verificationComments = VerificationComment::where('workflow_id','=',$id)->get(); 
+        $verificationComments = VerificationComment::where('workflow_id', '=', $id)->get(); 
         
         //delete comment replies of those comments
-        foreach ($verificationComments as $verificationComment)
-        {
-           CommentReply::where('verification_comment_id',$verificationComment->id)->delete(); 
+        foreach ($verificationComments as $verificationComment) {
+            CommentReply::where('verification_comment_id', $verificationComment->id)->delete();
         }
 
         //delete the verification comments
-        VerificationComment::where('workflow_id',$id)-> delete(); 
+        VerificationComment::where('workflow_id', $id)->delete();
 
         /*****delete the record about the loaded evidencio model for that workflow*****/
-        LoadedEvidencioModel::where('workflow_id',$id)->delete();
+        LoadedEvidencioModel::where('workflow_id', $id)->delete();
 
 
         /*****delete steps, options, fields, results*****/
-        $steps = Step::where('workflow_step_workflow_id',$id)->orderBy('workflow_step_level', 'desc')->get();
-        foreach ($steps as $step) 
-        {
+        $steps = Step::where('workflow_step_workflow_id', $id)->orderBy('workflow_step_level', 'desc')->get();
+        foreach ($steps as $step) {
             //references to Jaap DesignerSaveController, saveSteps function
             //$nextSteps = $step -> nextSteps() -> get();
             //foreach ($nextSteps as $nextStep) {
             //    $step->nextSteps()->detach($nextStep);
             //}
-            $previousSteps = $step -> previousSteps() -> get();
+            $previousSteps = $step->previousSteps()->get();
             foreach ($previousSteps as $previousStep) {
                 $step->previousSteps()->detach($previousStep);
             }
@@ -70,17 +68,17 @@ class MyWorkflowsController extends Controller
                 }
                 $field->delete();
             }
-            Result::where('step_id',$step->id)->delete(); 
+            Result::where('step_id', $step->id)->delete();
 
         }
-        Step::where('workflow_step_workflow_id',$id)->delete(); 
+        Step::where('workflow_step_workflow_id', $id)->delete();
 
         Workflow::destroy($id);
 
-    	$user_id = Auth::user()->id;
-	$workflows = Workflow::where('author_id','=',$user_id)->get();
-    	
-    	return redirect()->route('myworkflows');
+        $user_id = Auth::user()->id;
+        $workflows = Workflow::where('author_id', '=', $user_id)->get();
+
+        return redirect()->route('myworkflows');
     	//return view('/myworkflows',compact('workflows'));
     }
 }
